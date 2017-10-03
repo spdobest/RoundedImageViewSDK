53f9762d0774e75523a831877379f7dee4ae50c3
Sibaprasad Mohanty <neosoft.sibaprasad@angelbroking.com>
2017-10-03 17:15:53 +0530

    initial commits

---
+++ app/src/main/java/spm/spimagesdk/MainActivity.java
@@ -0,0 +1,13 @@
+package spm.spimagesdk;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
